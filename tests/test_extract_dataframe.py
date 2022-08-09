b87e782ecfc8c843e3ec354fffb24cd34dfdfd9c
nahomHmichael <85625708+nahomHmichael@users.noreply.github.com>
2022-08-10 00:45:51 +0300

    added 5 text to test_find_full_test method

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -57,7 +57,11 @@ class TestTweetDfExtractor(unittest.TestCase):
         )
 
     def test_find_full_text(self):
-        text = <provide a list of the first five full texts>
+        text =["RT @i_ameztoy: Extra random image (I):",
+               "RT @IndoPac_Info: #China's media explains the military reasons for each area of the drills in the #Taiwan Strait \n\nRead the labels in the pi…",\
+               "China even cut off communication, they don't anwer phonecalls from the US. But here clown @ZelenskyyUa enters the stage to ask #XiJinping to change Putin's mind.",\
+               "Putin to #XiJinping : I told you my friend, Taiwan will be a vassal state, including nukes, much like the Ukrainian model. I warned you... But it took Pelosi to open China's eyes.",\
+               "RT @ChinaUncensored: I’m sorry, I thought Taiwan was an independent country because it had its own government, currency, military, travel d…"]
 
         self.assertEqual(self.df.find_full_text(), text)
 
@@ -65,10 +69,9 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(
             self.df.find_sentiments(self.df.find_full_text()),
             (
-                <provide a list of the first five sentiment values>,
-                <provide a list of the first five polarity values>,
-            ),
-        )
+                [-1, 0, -1, 1, 0],
+                [0.0, -0.1, 0.0, 0.1, -6.938893903907228e-18]
+                ))
 
 
     def test_find_screen_name(self):
