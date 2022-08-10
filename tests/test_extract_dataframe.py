8f1815d3a975992371a2dbc8f398306f055cf0b0
nahomHmichael <85625708+nahomHmichael@users.noreply.github.com>
2022-08-10 12:17:03 +0300

    fixed test_find_full_text assertion issues

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -14,10 +14,26 @@ from extract_dataframe import TweetDfExtractor
 sampletweetsjsonfile = "sample.json"  # put here the path to where you placed the file e.g. ./sampletweets.json.
 _, tweet_list = read_json(sampletweetsjsonfile)
 
-columns = ['created_at', 'statuses_count', 'source', 'original_text', 'clean_text', 'sentiment', 'polarity', 'subjectivity', 'lang',
-               'favorite_count', 'retweet_count',
-               'original_author', 'screen_count', 'followers_count', 'friends_count', 'possibly_sensitive', 'hashtags',
-               'user_mentions', 'place', 'place_coord_boundaries']
+columns = ['created_at',
+           'statuses_count',
+           'source',
+           'original_text',
+           'clean_text',
+           'sentiment',
+           'polarity',
+           'subjectivity',
+           'lang',
+           'favorite_count',
+           'retweet_count',
+           'original_author',
+           'screen_count',
+           'followers_count',
+           'friends_count',
+           'possibly_sensitive',
+           'hashtags',
+           'user_mentions',
+           'place',
+           'place_coord_boundaries']
 
 class TestTweetDfExtractor(unittest.TestCase):
     """
@@ -39,13 +55,13 @@ class TestTweetDfExtractor(unittest.TestCase):
         )
 
     def test_find_full_text(self):
-        text = ["RT @i_ameztoy: Extra random image (I):\n''\n''Lets focus in one very specific zone of the western coast -&gt; Longjing ''District, Taichung #City, #Ta…'",
-                "RT @IndoPac_Info: #China's media explains the military reasons for each area of the drills in the #Taiwan Strait \n\nRead the labels in the pi…", \
-                "China even cut off communication, they don't anwer phonecalls from the US. But here clown @ZelenskyyUa enters the stage to ask #XiJinping to change Putin's mind.", \
-                "Putin to #XiJinping : I told you my friend, Taiwan will be a vassal state, including nukes, much like the Ukrainian model. I warned you... But it took Pelosi to open China's eyes.", \
-                "RT @ChinaUncensored: I’m sorry, I thought Taiwan was an independent country because it had its own government, currency, military, travel d…"]
-
-        self.assertEqual(self.df.find_full_text(), text)
+        text = ['RT @i_ameztoy: Extra random image (I):\n\nLets focus in one very specific zone of the western coast -&gt; Longjing District, Taichung #City, #Ta…',
+                "RT @IndoPac_Info: #China's media explains the military reasons for each area of the drills in the #Taiwan Strait\n\nRead the labels in the pi…",
+                "China even cut off communication, they don't anwer phonecalls from the US. But here clown @ZelenskyyUa enters the stage to ask #XiJinping to change Putin's mind.",
+                "Putin to #XiJinping : I told you my friend, Taiwan will be a vassal state, including nukes, much like the Ukrainian model. I warned you... But it took Pelosi to open China's eyes.",
+                'RT @ChinaUncensored: I’m sorry, I thought Taiwan was an independent country because it had its own government, currency, military, travel d…']
+        text_original,_ = self.df.find_full_text()
+        self.assertEqual(text_original, text)
 
     def test_find_sentiments(self):
         self.assertEqual(
@@ -53,7 +69,7 @@ class TestTweetDfExtractor(unittest.TestCase):
             (
                 [-1, 0, -1, 1, 0],
                 [0.0, -0.1, 0.0, 0.1, -6.938893903907228e-18]
-            ))
+            ),)
 
     def test_find_screen_name(self):
         name = ['i_ameztoy', 'ZIisq', 'Fin21Free', 'Fin21Free', 'VizziniDolores']
